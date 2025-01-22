8d7887ecc891894564a3d54dca51d79cd1898a39
ajayproctur <ajay.kumar@proctur.com>
2025-01-22 14:04:22 +0530

    Property of triangle

--- First.java
+++ First.java
@@ -2,5 +2,6 @@ public class First {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is a statement added from vs code");
+        System.out.println("Each side of triangle is less than the sum of two other sides");
     }
 }
