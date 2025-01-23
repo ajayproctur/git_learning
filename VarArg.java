3bb9a6cffa0dd919d102347ee2161220de9c68c1
ajayproctur <ajay.kumar@proctur.com>
2025-01-23 16:22:03 +0530

    Save VarArg Program

---
+++ VarArg.java
@@ -0,0 +1,14 @@
+public class VarArg {
+
+    public void methodOne(int... arg){
+
+    }
+
+    public void methodOne(int a,float...b){
+
+    }
+
+    public void methodOne(int a,int... arg){
+
+    }
+}
