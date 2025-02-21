e2a4dfffabf85c9b4e3003717f093bf976656772
ajayproctur <ajay.kumar@proctur.com>
2025-02-21 18:00:12 +0530

    contain response ent

--- EnumDemoLec7.java
+++ EnumDemoLec7.java
@@ -33,3 +33,71 @@ public class EnumDemoLec7 {
         }
     }
 }
+// package com.proc.exam.util;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+// import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+// import com.fasterxml.jackson.databind.annotation.JsonNaming;
+// import com.proc.exam.exception.ErrorResponse;
+
+// import lombok.Data;
+
+// @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+// @Data
+// public class ResponseEntity<T> {
+
+//     private String             statusCode;
+//     private String             message;
+//     T                          response;
+//     private String             stackTrace;
+//     List<ErrorResponse>        errorResponse       = new ArrayList<>();
+//     private String             requestId;
+//     public static final String SUCCESS_STATUS_CODE = "200";
+//     public static final String SUCCESS_STATUS_MSG  = "OK";
+//     public static final String ERROR_STATUS_CODE   = "500";
+//     public static final String ERROR_STATUS_MSG    = "Internal Server Error";
+
+//     public ResponseEntity(T response) {
+//         this(SUCCESS_STATUS_CODE, SUCCESS_STATUS_MSG, response);
+//     }
+
+//     public ResponseEntity(String statusCode, String message, T response) {
+//         super();
+//         this.statusCode = statusCode;
+//         this.message = message;
+//         this.response = response;
+//     }
+
+//     public ResponseEntity(String statusCode, String message, String requestId, T response) {
+//         super();
+//         this.statusCode = statusCode;
+//         this.message = message;
+//         this.response = response;
+//         this.requestId = requestId;
+//     }
+
+//     public ResponseEntity(List<ErrorResponse> errorResponse, String stackTrace) {
+//         this(errorResponse != null && !errorResponse.isEmpty() && errorResponse.get(0).getErrorCode() > 0 ? errorResponse.get(0).getErrorCode() + "" : ERROR_STATUS_CODE,
+//                 errorResponse != null && !errorResponse.isEmpty() && Utilities.isNotBlank(errorResponse.get(0).getErrorMessage()) ? errorResponse.get(0).getErrorMessage() : ERROR_STATUS_MSG,
+//                 null);
+
+//         this.errorResponse = errorResponse;
+//         this.stackTrace = stackTrace;
+//     }
+
+//     public ResponseEntity(String stackTrace, String requestId) {
+//         this(ERROR_STATUS_CODE, ERROR_STATUS_MSG, null);
+//         this.requestId = requestId;
+//         this.stackTrace = stackTrace;
+//     }
+
+//     public ResponseEntity(List<ErrorResponse> errorResponse, String stackTrace, String requestId) {
+//         this(ERROR_STATUS_CODE, ERROR_STATUS_MSG, null);
+//         this.errorResponse = errorResponse;
+//         this.stackTrace = stackTrace;
+//         this.requestId = requestId;
+//     }
+
+// }
