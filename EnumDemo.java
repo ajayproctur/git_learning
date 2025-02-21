2fb5e2c6f4798c4e94f94d55867a3c117e0cf692
ajayproctur <ajay.kumar@proctur.com>
2025-02-21 15:56:44 +0530

    lecture 6 durgasoft

--- EnumDemo.java
+++ EnumDemo.java
@@ -1,5 +1,35 @@
+
+
 enum B{
     KF,KO,RC,FO;
+}
+enum C{
+    A,B,C,E;//will get compile time error if we remove semicolon from here
+    public static void main(String[] args) {
+        
+    }
+}
+enum D{
+    A,B,C,D;
+    public void methodOne(){
+
+    }
+}
+
+enum E{
+    ;
+    public  void methodTwo(){
+
+    }
+}
+
+enum F{
+    // public void methodOne(){} invalid
+}
+
+enum P{
+    // public void methodOne(){}
+    // A,B,C,D->Enum constant must be declared in the first line
 }
 public class EnumDemo {
     public static void main(String[] args) {
