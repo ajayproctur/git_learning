462b70530f3f9977c76775dd2305213b83ab2e4c
ajayproctur <ajay.kumar@proctur.com>
2025-02-21 14:19:00 +0530

    is same binary tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -226,5 +226,11 @@ public class BinaryTree {
         // bfs(root);
         diameterOfBinaryTree(root);
     }
-
+public boolean isSameTree(Node p,Node q){
+ 
+    if(p==null && q==null) return true;
+    if(p==null || q==null) return false;
+    if(p.val!=q.val) return false;
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
 }
