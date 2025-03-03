5950606c0e4da282b214a3243b19bce9cb1db18f
ajayproctur <ajay.kumar@proctur.com>
2025-03-03 11:16:07 +0530

    Lowest comman ancestor

--- BinaryTree.java
+++ BinaryTree.java
@@ -199,6 +199,25 @@ public class BinaryTree {
         return isBalanced(root.left) && isBalanced(root.right);
     }
 
+    //Lowest Comman Ancestor
+    public Node lowestCommonAncestor(Node root, Node p, Node q) {
+        if(p==root || q==root) return root;
+        if(p==q) return p;
+        boolean leftp=contains(root.left,p);
+        boolean rightq=contains(root.right,q);
+
+        if((leftp && rightq)||(!leftp && !rightq)) return root;
+        if(leftp && !rightq) return lowestCommonAncestor(root.left, p, q);
+        if(!leftp && rightq) return lowestCommonAncestor(root.right, p, q);
+        return null;
+    }
+
+    public boolean contains(Node root,Node node){
+        if(root==null) return false;
+        if(root==node) return true;
+        return contains(root.left, node) || contains(root.right, node);
+    }
+
     public static void main(String[] args) {
         Node root = new Node(-2);
         Node a = new Node(-4);
@@ -226,12 +245,19 @@ public class BinaryTree {
         // bfs(root);
         diameterOfBinaryTree(root);
     }
-public boolean isSameTree(Node p,Node q){
- 
-    if(p==null && q==null) return true;
-    if(p==null || q==null) return false;
-    if(p.val!=q.val) return false;
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-}
+
+    public boolean isSameTree(Node p, Node q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 
 }
